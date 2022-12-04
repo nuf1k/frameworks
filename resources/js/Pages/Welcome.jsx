8443cc7ebafe56ac1c3b49881eb6b67649ed4ca5
martenj <marten@jalakas.ee>
2022-12-04 18:52:29 +0200

    Remove registration

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -16,12 +16,6 @@ export default function Welcome(props) {
                                 Log in
                             </Link>
 
-                            <Link
-                                href={route('register')}
-                                className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                            >
-                                Register
-                            </Link>
                         </>
                     )}
                 </div>
